9b91e5044721288cfc095f4bcf37c0c77b433238
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-01-20 14:40:06 -0500

    fix: moved cookie parser to express config

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -1,7 +1,8 @@
-import { expressCorsOpts } from './cors.config.js';
 import express from 'express';
 import cors from 'cors';
+import { expressCorsOpts } from './cors.config.js';
+import cookieParser from 'cookie-parser';
 
-const expressConfig = [cors(expressCorsOpts), express.json(), express.urlencoded({ extended: true })];
+const expressConfig = [cors(expressCorsOpts), express.json(), express.urlencoded({ extended: true }), cookieParser()];
 
 export default expressConfig;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,7 @@
 import expressConfig from '../config/express.config.js';
 import routes from '../api/routes/index.js';
 import { errorHandler } from '../middleware/errorHandler.middleware.js';
-import cookieParser from 'cookie-parser';
 
-const config: Array<any> = [...expressConfig, routes, errorHandler, cookieParser];
+const config: Array<any> = [...expressConfig, routes, errorHandler];
 
 export default config;
