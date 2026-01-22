4425d8a5ff6f60e7d7d8a15515ad56f1597e7671
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-01-22 13:26:28 -0500

    config: edited cookie config to always be sameSite: none

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -1,10 +1,8 @@
 const cookieConfig: Object = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
+    sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000,
-
-    // only necessary since using custom domain with gh-pages
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
 };
 
 export default cookieConfig;
