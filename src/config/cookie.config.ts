e9b8b19b30fc167876d74abb6af0876e7e5b062d
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-02-03 13:37:31 -0500

    refactor: made cookie secure/sameSite depend on environment

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -1,7 +1,11 @@
+import 'dotenv/config';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const cookieConfig: Object = {
     httpOnly: true,
-    secure: true,
-    sameSite: 'none', // Required for render and gh-pages
+    secure: isProduction,
+    sameSite: isProduction ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000,
 };
 
