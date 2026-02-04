246835a1fcd382356285770260ad963432225b57
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-02-04 14:53:17 -0500

    ci: regenerated migrations

--- prisma/schema/migrations/20260126194424_idempotency_log/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "idempotency_logs" (
-    "key" TEXT NOT NULL,
-    "user_id" UUID NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "idempotency_logs_pkey" PRIMARY KEY ("key")
-);
---
+++ prisma/schema/migrations/20260204195114_init/migration.sql
@@ -0,0 +1,78 @@
+-- CreateEnum
+CREATE TYPE "CounterType" AS ENUM ('PERSONAL', 'SHARED');
+
+-- CreateEnum
+CREATE TYPE "ShareStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- CreateEnum
+CREATE TYPE "UserTier" AS ENUM ('PREMIUM', 'BASIC');
+
+-- CreateTable
+CREATE TABLE "counters" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+    "color" TEXT,
+    "type" "CounterType" NOT NULL DEFAULT 'PERSONAL',
+    "invite_code" TEXT,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "counters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "counter_shares" (
+    "id" TEXT NOT NULL,
+    "status" "ShareStatus" NOT NULL DEFAULT 'PENDING',
+    "counter_id" TEXT NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "counter_shares_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "idempotency_logs" (
+    "key" TEXT NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "idempotency_logs_pkey" PRIMARY KEY ("key")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" TEXT,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "tier" "UserTier" NOT NULL DEFAULT 'BASIC',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "counters_invite_code_key" ON "counters"("invite_code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "counter_shares_counter_id_user_id_key" ON "counter_shares"("counter_id", "user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- AddForeignKey
+ALTER TABLE "counters" ADD CONSTRAINT "counters_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "counter_shares" ADD CONSTRAINT "counter_shares_counter_id_fkey" FOREIGN KEY ("counter_id") REFERENCES "counters"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "counter_shares" ADD CONSTRAINT "counter_shares_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
